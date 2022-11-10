74687a8ae2eb3153a515dd78201620526da06cca
Petrik Diak <diak@petrik.hu>
2022-11-10 18:20:36 +0100

    Kész van

--- src/hu/petrik/peopleclientconsole/Main.java
+++ src/hu/petrik/peopleclientconsole/Main.java
@@ -1,7 +1,39 @@
 package hu.petrik.peopleclientconsole;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+    private static String url = "https://retoolapi.dev/SF77IG/people";
+    public static void main(String[] args) {
+        try {
+            newPeopleFromConsole();
+            printPeople();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
+
+
+    private static void newPeopleFromConsole() throws IOException{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adja meg a nevét");
+        String name = sc.nextLine();
+        System.out.println("Adja meg az email címét");
+        String email = sc.nextLine();
+        System.out.println("Adja meg az életkorát");
+        String age = sc.nextLine();
+        String json = String.format("{\"name\": \"%s\",\"email\": \"%s\",\"age\": \"%s\"}"
+                ,name,email,age);
+        Response response = RequestHandler.post(url,json);
+        System.out.println(response.getContent());
+    }
+
+
+    private static void printPeople() throws IOException{
+        Response response = RequestHandler.get(url);
+        System.out.println(response.getContent());
+    }
+
+
 }
--- src/hu/petrik/peopleclientconsole/RequestHandler.java
+++ src/hu/petrik/peopleclientconsole/RequestHandler.java
@@ -1,11 +1,9 @@
 package hu.petrik.peopleclientconsole;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.Buffer;
 
 public final class RequestHandler {
 
@@ -14,22 +12,43 @@ public final class RequestHandler {
     }
 
     public static Response get(String url) throws IOException {
-        URL urlOBJ = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) urlOBJ.openConnection();
-        connection.setConnectTimeout(10000);
-        connection.setReadTimeout(10000);
-        connection.setRequestProperty("Accept", "application/json");
+        HttpURLConnection connection = setUpConnection(url);
 
         connection.setRequestMethod("GET");
 
+        return getResponse(connection);
+
+    }
+
+    public static Response post(String url, String data) throws IOException {
+        HttpURLConnection connection = setUpConnection(url);
+
+        connection.setRequestMethod("POST");
+
+        connection.setRequestProperty("Content-Type","application/json");
+        connection.setDoOutput(true);
+        OutputStream os = connection.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os));
+        writer.write(data);
+        writer.flush();
+        writer.close();
+        os.close();
+
+        return getResponse(connection);
+    }
+
+    private static Response getResponse(HttpURLConnection connection) throws IOException {
         int responseCode = connection.getResponseCode();
-        InputStream is = connection.getInputStream();
+        InputStream is = null;
+        if (responseCode < 400){
+            is = connection.getInputStream();
+        }else{
+            is = connection.getErrorStream();
+        }
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         StringBuilder builder = new StringBuilder();
-        /*for(String line = br.readLine(); line != null; line = br.readLine());*/
-
         String line = br.readLine();
-        while (br != null){
+        while (line != null) {
             builder.append(line).append(System.lineSeparator());
             line = br.readLine();
         }
@@ -37,7 +56,15 @@ public final class RequestHandler {
         is.close();
         String content = builder.toString().trim();
         return new Response(responseCode, content);
+    }
 
+    private static HttpURLConnection setUpConnection(String url) throws IOException {
+        URL urlOBJ = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlOBJ.openConnection();
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+        connection.setRequestProperty("Accept", "application/json");
+        return connection;
     }
 
 }
